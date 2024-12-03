bf64bd2d0edcfc9a110f3937c114b2859ebd999e
kylelton <166498512+kylelton@users.noreply.github.com>
2024-12-03 01:04:14 -0800

    LoginTable.java

--- src/LoginTable.java
+++ src/LoginTable.java
@@ -2,11 +2,15 @@
  * Handles user authentication and login credential management.
  */
 public class LoginHashTable {
+	
+	private HashTable<User> loginTable;
 
     /**
      * Default constructor for LoginHashTable.
      */
-    public LoginHashTable() {}
+    public LoginHashTable() {
+    	loginTable = new HashTable<>(15); //Size 10, suitable size? Might need more for when we create new account
+    }
 
     /**
      * Authenticates a user by their username and password.
@@ -16,14 +20,24 @@ public class LoginHashTable {
      * @return True if the credentials are valid, otherwise false.
      */
     public boolean authenticate(String username, String password) {
-        return false;
+        User user = new User(username, password); //need default constructor for user for name and pass
+        return loginTable.contains(user); //check if loginTable contains user
     }
 
     /**
      * Adds a user to the login hash table.
-     *
-     * @param username The username of the new user.
-     * @param password The password of the new user.
+     * 
+     * @param User, contains name and password
      */
-    public void addUser(String username, String password) {}
+    public void addUser(User user) {
+    	loginTable.add(user);
+    }
+    
+    /**
+     * Retrieves full User after authentication
+     * 
+     */
+    public User getUser(User user) {
+    	return loginTable.get(user);
+    }
 }
