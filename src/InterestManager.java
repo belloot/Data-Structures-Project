a77f7fdf65362f37881908131e303bae663457fb
kylelton <166498512+kylelton@users.noreply.github.com>
2024-12-03 01:03:37 -0800

    InterestManager.java

--- src/InterestManager.java
+++ src/InterestManager.java
@@ -1,14 +1,65 @@
 /**
  * Manages user interests and provides search functionality based on interests.
  */
-public class InterestManager {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
+public class InterestManager {
+	
+	private HashTable<Interest> interestTable; //Stores Interest objects
+	private ArrayList<BST<User>> usersByInterest; //Indexed by interest id
+	private FullNameComparator fullNameCmp = new FullNameComparator();
+	
     /**
      * Default constructor for InterestManager.
      */
     public InterestManager() {
+    	interestTable = new HashTable<>(10);
+    	usersByInterest = new ArrayList<>();
     }
+    
+    // get the HashTable of Interest objects
+    public HashTable<Interest> getInterestTable() {
+		return this.interestTable;
+	}
+    
+    /**
+     * Adds a new interest
+     * 
+     * @param newInterest the new interest
+     */
+    public void addInterest(String newInterestStr) {
+    	Interest newInterest = new Interest(usersByInterest.size(), newInterestStr); //maybe random number for new interest id?
+    	if(!interestTable.contains(newInterest)) { //check if a completely new interest
+    		interestTable.add(newInterest);
+    		usersByInterest.add(new BST<User>()); //add new BST user to store users with this interest
+    	}
+    }
+    
+    /**
+     * Adds a user to an interest
+     * 
+     * @param newInterest the new interest
+     */
+    public void addUserInterest(User user, String interest) {
+    	Interest tempInterest = new Interest(0, interest); //no need to care about id, use this to search for existing interest in interestTable
+    	Interest actualInterest = interestTable.get(tempInterest); //Check if already exists in interestTable
+    	if(actualInterest == null) {//interest doesnt exist
+    		addInterest(interest); //add interest if doesnt exist
+    		actualInterest = interestTable.get(tempInterest); //after adding, get the interest
+    	}
+    	usersByInterest.get(actualInterest.getId()).insert(user, fullNameCmp); //get the BST that contains users who have this interest id
 
+    	//User tempUser = new User(); //create a comparator that will be used to keep the BST ordering when inserting the user
+    	//Comparator<User> userComparator = tempUser.new NameComparator(); // past FullNameComparator inside this file can use it, no need to make new User
+
+        // May have problems
+
+
+    	//userBST.insert(user, userComparator); //add user to the BST of users with same interest id
+    }
+    
     /**
      * Searches for users who share a specific interest.
      *
@@ -16,10 +67,41 @@ public class InterestManager {
      * @return A list of User objects who share the specified interest.
      */
     public ArrayList<User> searchUsersByInterest(String interest) {
-        return null;
+    	Interest tempInterest = new Interest(0, interest);
+    	Interest actualInterest = interestTable.get(tempInterest);
+    	if(actualInterest == null) {
+    		return new ArrayList<>(); //No users found
+    	}
+    	BST<User> userBST = usersByInterest.get(actualInterest.getId());
+    	ArrayList<User> usersList = new ArrayList<>();
+    	userBST.inOrderTraversal(usersList);
+    	return usersList;
     }
 
-    public ArrayList<User> createInterestArray(Scanner s) {
-        return null;
-    }
+    // prompt the user to enter all of their interests
+    // handle all the interests:
+    // putting all users with same existing interest in the correct Bst
+    // if there are new interests, add to the interests arraylist
+    // return arraylist of users so that user can be constructed
+
+   public ArrayList<Interest> createInterestArray(Scanner s) {
+      return null;
+  }
+   
+   class FullNameComparator implements Comparator<User> {
+       /**
+        * Compares the two Users by first names
+        * uses the String compareTo method to make the comparison
+        * 
+        * @param user1 the first User
+        * @param user2 the second User
+        * @return The comparison.
+        */
+       @Override
+       public int compare(User user1, User user2) {
+       	String user1FullName = user1.getFirstName() + user1.getLastName();
+       	String user2FullName = user2.getFirstName() + user2.getLastName();
+           return user1FullName.compareTo(user2FullName);
+       }
+   }
 }
